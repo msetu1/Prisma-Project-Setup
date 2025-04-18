fe09f81fc62cd33b47cd31cc7d5236a3c5877aa9
msetu1 <msetu5763@gmail.com>
2025-04-18 20:27:23 +0600

    dat get done

--- src/find.ts
+++ src/find.ts
@@ -5,6 +5,7 @@ const prisma = new PrismaClient();
 const main = async () => {
   // find all data
   const getAllFromDB = await prisma.post.findMany();
+  console.log(getAllFromDB);
   const findFirst = await prisma.post.findFirst({
     where: {
       id: 1,
