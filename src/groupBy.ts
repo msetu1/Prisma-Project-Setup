3f54ce64e9ede3b13a02104c961c836256c8b969
msetu1 <msetu5763@gmail.com>
2025-05-01 22:17:26 +0600

    groupby

---
+++ src/groupBy.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const groupBy = async () => {
+  const groupPost = await prisma.post.groupBy({
+    by: ["published"],
+    _count: {
+      title: true,
+    },
+    having: {
+      authorId: {
+        _sum: {
+          gt: 1,
+        },
+      },
+    },
+  });
+  console.log(groupPost);
+};
+
+groupBy();
